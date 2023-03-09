ab248bfed4d717b508052652833399293d84d03d
shiliu <319227835@qq.com>
2023-03-09 16:06:44 +0800

    十六个人的学习项目初始化

---
+++ spring/src/main/java/cn/edu/shiliu/spring/Main.java
@@ -0,0 +1,13 @@
+package cn.edu.shiliu.spring;
+
+/**
+ * @author ${USER}
+ * @version 1.0
+ * @description: TODO
+ * @date ${DATE} ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
