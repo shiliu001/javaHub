081aaf08120a81bdce60a96da698eef663182251
shiliu <319227835@qq.com>
2023-03-10 11:39:41 +0800

    springboot项目初始化测试 创建文件，初始化完成

---
+++ springboot/src/main/java/cn/edu/shiliu/springboot/a.java
@@ -0,0 +1,14 @@
+package cn.edu.shiliu.springboot;
+
+/**
+ * @author ZQ01
+ * @version 1.0
+ * @description: TODO
+ * @date 2023/3/10 9:19
+ */
+public class a {
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+}
